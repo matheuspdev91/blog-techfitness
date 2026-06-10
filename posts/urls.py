b0c89229e7f9b21a199d7c377986a378084a44f6
MatheusPD-dev <mpdev34@gmail.com>
2026-06-10 12:50:56 -0300

    Adicionando pagia ao blog dia 10/06/2026

--- posts/urls.py
+++ posts/urls.py
@@ -13,6 +13,26 @@ urlpatterns =[
     path('dislike/<slug:slug>/', dislike_post, name='dislike_post'),
     path('post/<slug:slug>/comentar/', adicionar_comentario, name='adicionar_comentario'),
     path('carregar/', carregar_dados),
+    
+]
+
+from django.urls import path
+from .views import lista_posts, post_detail, post_por_categoria, like_post, dislike_post, adicionar_comentario
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import carregar_dados
+from . import views
+
+
+urlpatterns =[
+    path('', lista_posts, name ='lista_posts'),
+    path('post/<slug:slug>/', post_detail, name='post_detail'),
+    path('categoria/<slug:slug>/', post_por_categoria, name='posts_categoria'),
+    path('likes/<slug:slug>/', like_post, name='like_post'),
+    path('dislike/<slug:slug>/', dislike_post, name='dislike_post'),
+    path('post/<slug:slug>/comentar/', adicionar_comentario, name='adicionar_comentario'),
+    path('carregar/', carregar_dados),
+    path('sobre/', views.sobre,name='sobre')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- posts/views.py
+++ posts/views.py
@@ -99,3 +99,109 @@ def carregar_dados(request):
         return HttpResponse("Dados carregados!")
     except Exception as e:
         return HttpResponse(f"Erro real: {str(e)}")
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post
+from django.core.paginator import Paginator
+from django.db.models import Q
+from .models import Post, Categoria, Comentario
+from django.core.management import call_command
+from django.http import HttpResponse
+
+
+
+
+def post_por_categoria(request, slug):
+    categoria = get_object_or_404(Categoria, slug=slug)
+
+    posts =  Post.objects.filter(categoria=categoria).order_by('-criado_em')
+    total_posts = posts.count()
+
+
+    paginator = Paginator(posts, 5)
+    page_number = request.GET.get("page", 1)
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, "posts/lista_posts.html",{
+        "page_obj": page_obj,
+        "categoria": categoria
+    })
+
+
+def lista_posts(request):
+
+    query = request.GET.get('q', '')
+
+    posts = Post.objects.all().order_by('-criado_em')
+    print('total posts:', posts.count())
+
+    if query:
+        posts = posts.filter(
+            Q(titulo__icontains=query) |
+            Q(conteudo__icontains=query)
+        )
+    
+    paginator = Paginator(posts, 5)
+
+    page_number = request.GET.get('page', 1)
+    page_obj = paginator.get_page(page_number)
+
+
+    return render(request, 'posts/lista_posts.html', {
+        'page_obj': page_obj,
+        'query': query
+    })
+
+
+def post_detail(request, slug):
+
+    post = get_object_or_404(Post, slug=slug)
+
+    return render(request, 'posts/post_detail.html', {
+        'post': post
+    })
+
+def like_post(request, slug):
+    post =  get_object_or_404(Post, slug=slug)
+    post.likes += 1
+    post.save()
+    return redirect('post_detail', slug=slug)
+
+def dislike_post(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    post.dislikes += 1
+    post.save()
+    return redirect('post_detail', slug=slug)
+
+def adicionar_comentario(request, slug):
+    post =  get_object_or_404(Post, slug=slug)
+
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        conteudo = request.POST.get('conteudo')
+
+        if nome and conteudo:
+            Comentario.objects.create(
+                post=post,
+                nome=nome,
+                conteudo=conteudo
+            )
+
+    return redirect('post_detail', slug=slug)
+
+
+
+
+
+def carregar_dados(request):
+    from django.core.management import call_command
+    from django.http import HttpResponse
+
+    try:
+        call_command('loaddata', 'dados.json')
+        return HttpResponse("Dados carregados!")
+    except Exception as e:
+        return HttpResponse(f"Erro real: {str(e)}")
+    
+
+def sobre(request):
+    return render(request, 'posts/sobre.html')
